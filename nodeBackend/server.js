5b15ad56ca44e77fb51a3d90de3d67722e0c0c7f
ayushjhax <ayushkmrjha@yahoo.com>
2025-05-16 19:39:44 +0530

    Deployed

--- nodeBackend/server.js
+++ nodeBackend/server.js
@@ -36,7 +36,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://rostrafi.fun/'],
     methods: ['GET', 'POST'],
     credentials: true,
   },
